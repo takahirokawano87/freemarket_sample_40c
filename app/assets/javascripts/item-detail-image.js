c8eb4a55c1f74eebd279087f9521f45f09c05deb
hanehane3710 <hanehane0464@gmail.com>
2018-12-17 15:03:55 +0900

    商品画像のスライドのリンクにopacityの変化をつけた

--- app/assets/javascripts/item-detail-image.js
+++ app/assets/javascripts/item-detail-image.js
@@ -2,21 +2,33 @@ $(document).ready(function(){
   $('.item-image2').on('mouseover',function(){
     $('.active-image').children().animate({
       marginLeft: "-300px"
-    },400);
+    });
+  });
+  $('.item-image2').on('mouseover mouseout',function(){
+    $(this).toggleClass('add-opacity');
   });
   $('.item-image3').on('mouseover',function(){
     $('.active-image').children().animate({
       marginLeft: "-600px"
-    })
+    });
+  });
+  $('.item-image3').on('mouseover mouseout',function(){
+    $(this).toggleClass('add-opacity');
   });
   $('.item-image4').on('mouseover',function(){
     $('.active-image').children().animate({
       marginLeft: "-900px"
-    })
+    });
+  });
+  $('.item-image4').on('mouseover mouseout',function(){
+    $(this).toggleClass('add-opacity');
   });
   $('.item-image1').on('mouseover',function(){
     $('.active-image').children().animate({
       marginLeft: "0px"
     })
+  });
+  $('.item-image1').on('mouseover mouseout',function(){
+    $(this).toggleClass('add-opacity');
   });
 });
