afafdc762a4e02248aa5599e334114e7cb91c9ea
matsumoto <44424144+mgen3104@users.noreply.github.com>
2018-12-17 12:45:29 +0900

    新規登録情報にニックネームを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -13,4 +14,9 @@ class ApplicationController < ActionController::Base
       username == "admin" && password == "2222"
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
 end
