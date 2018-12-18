4a9596268689485559e3ca4d309a37d790d51608
hanehane3710 <hanehane0464@gmail.com>
2018-12-18 13:53:04 +0900

    商品画像スライドのjsのリファクタリング
    それに伴い画像に新たにclassを付与

--- app/assets/javascripts/item-detail-image.js
+++ app/assets/javascripts/item-detail-image.js
@@ -4,31 +4,22 @@ $(document).ready(function(){
       marginLeft: "-300px"
     });
   });
-  $('.item-image2').on('mouseover mouseout',function(){
-    $(this).toggleClass('add-opacity');
-  });
   $('.item-image3').on('mouseover',function(){
     $('.active-image').children().animate({
       marginLeft: "-600px"
     });
-  });
-  $('.item-image3').on('mouseover mouseout',function(){
-    $(this).toggleClass('add-opacity');
   });
   $('.item-image4').on('mouseover',function(){
     $('.active-image').children().animate({
       marginLeft: "-900px"
     });
   });
-  $('.item-image4').on('mouseover mouseout',function(){
-    $(this).toggleClass('add-opacity');
-  });
   $('.item-image1').on('mouseover',function(){
     $('.active-image').children().animate({
       marginLeft: "0px"
     })
   });
-  $('.item-image1').on('mouseover mouseout',function(){
+  $('.item-slide').on('mouseover mouseout',function(){
     $(this).toggleClass('add-opacity');
   });
 });
