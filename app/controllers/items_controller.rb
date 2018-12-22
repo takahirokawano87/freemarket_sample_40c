969d35a8dcbe32e16f86daa260051af2c400ed68
hanehane3710 <hanehane0464@gmail.com>
2018-12-22 17:05:15 +0900

    Itemsコントローラーにset_itemメソッドを記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show,:buy]
+
   def index
   end
 
@@ -15,7 +17,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def buy
@@ -25,6 +26,10 @@ private
 
   def item_params
     params.require(:item).permit(:name, :description, :first_category_id, :second_category_id, :third_category_id, :size_id, :brand_id, :condition, :price, image_attributes: [:image1, :image2, :image3, :image4], delivery_attributes: [:fee, :kind, :area, :delivery_days]).merge(seller_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
