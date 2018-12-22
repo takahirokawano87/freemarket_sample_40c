ab743da7868fe1e456fee4329aadb680bc910a67
matsumoto <44424144+mgen3104@users.noreply.github.com>
2018-12-22 12:31:52 +0900

    写真を４つ登録できるようになった

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    binding.pry
     @item = Item.new(item_params)
     @item.save!
     redirect_to root_path
@@ -24,7 +23,7 @@ class ItemsController < ApplicationController
 private
 
   def item_params
-    params.require(:item).permit(:name, :description, :first_category_id, :second_category_id, :third_category_id, :size_id, :brand_id, :condition, :price, image_attributes: [:image1], delivery_attributes: [:fee, :kind, :area, :delivery_days]).merge(seller_id: current_user.id)
+    params.require(:item).permit(:name, :description, :first_category_id, :second_category_id, :third_category_id, :size_id, :brand_id, :condition, :price, image_attributes: [:image1, :image2, :image3, :image4], delivery_attributes: [:fee, :kind, :area, :delivery_days]).merge(seller_id: current_user.id)
   end
 
 end
