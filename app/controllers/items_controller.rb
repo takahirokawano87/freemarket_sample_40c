6f732720b5c4775790e443c7b5f6426d601ba7a4
hanehane3710 <hanehane0464@gmail.com>
2018-12-23 15:58:48 +0900

    出品者の他の商品へのリンク実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,12 +17,15 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @seller_id = Item.where(seller_id: @item.seller_id)
+    @previous = @seller_id.where('id < ?', @item.id).first
+    @next = @seller_id.where('id > ?', @item.id).first
   end
 
   def buy
   end
 
-private
+  private
 
   def item_params
     params.require(:item).permit(:name, :description, :first_category_id, :second_category_id, :third_category_id, :size_id, :brand_id, :condition, :price, image_attributes: [:image1, :image2, :image3, :image4], delivery_attributes: [:fee, :kind, :area, :delivery_days]).merge(seller_id: current_user.id)
