df37e2653fd636fe328320d54ff21f80cf841514
hanehane3710 <hanehane0464@gmail.com>
2018-12-25 16:04:51 +0900

    商品の購買状況でのボタンの機能を修正
    seller_id ではなく　buyer_idに値があるかで購買状況を判断するようにした

--- app/assets/javascripts/item-detail-btn.js
+++ app/assets/javascripts/item-detail-btn.js
@@ -1,15 +1,10 @@
 $(document).ready(function(){
-  var seller_id = $('.seller-id').val();
+  var bayer_id = $('.buyer-id').val();
   var btn = ('.item-buy-btn');
-  if (seller_id){
-    console.log($(seller_id));
-    $(btn).removeClass('disabled')
-    $(btn).addClass('sell');
-  }else{
-    console.log($(seller_id));
+  if (bayer_id){
     $(btn).text('売り切れました');
     $(btn).addClass('disabled').removeClass('sell').on('click',function(){
       return false;
     });
-  };
+  }
 });
