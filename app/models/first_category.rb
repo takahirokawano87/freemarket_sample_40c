ad92eb5c9dc7c1439564b6bbec0b621614580459
hanehane3710 <hanehane0464@gmail.com>
2018-12-25 14:49:25 +0900

    モデル間のアソシエーションを組んだ
    カテゴリー間及びitemsテーブルとのアソシエーション
    sizeがitemにhas_many

--- app/models/first_category.rb
+++ app/models/first_category.rb
@@ -1,2 +1,4 @@
 class FirstCategory < ApplicationRecord
+  has_many :item
+  has_many :second_category
 end
--- app/models/second_category.rb
+++ app/models/second_category.rb
@@ -1,2 +1,5 @@
 class SecondCategory < ApplicationRecord
+  has_many :item
+  belongs_to :first_category
+  has_many :third_category
 end
