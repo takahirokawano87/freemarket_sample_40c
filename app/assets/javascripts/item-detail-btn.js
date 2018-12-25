1d4f8124a7c00275ca7c3386b16ee7d5bf651739
hanehane3710 <hanehane0464@gmail.com>
2018-12-25 14:56:20 +0900

    $(seller_id)を　seller_idに変更
    変数をjqueryオブジェクトにしていたのをjsの変数に修正

--- app/assets/javascripts/item-detail-btn.js
+++ app/assets/javascripts/item-detail-btn.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
   var seller_id = $('.seller-id').val();
   var btn = ('.item-buy-btn');
-  if ($(seller_id)){
+  if (seller_id){
+    console.log($(seller_id));
+    $(btn).removeClass('disabled')
     $(btn).addClass('sell');
   }else{
+    console.log($(seller_id));
     $(btn).text('売り切れました');
     $(btn).addClass('disabled').removeClass('sell').on('click',function(){
       return false;
