c058e1e90238f37864016b3c56e97da3d0e4807d
hanehane3710 <hanehane0464@gmail.com>
2019-01-04 15:36:19 +0900

    Deviseのソースコードから不要なコードを削除した

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -28,8 +28,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
         @personal_info.telephone = params[:user][:personal_info][:telephone]
 
         resource.save
-        yield resource if block_given?
-        if resource.persisted?
           if resource.active_for_authentication?
             if @personal_info.save
               set_flash_message! :notice, :signed_up
@@ -43,11 +41,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
             expire_data_after_sign_in!
             respond_with resource, location: after_inactive_sign_up_path_for(resource)
           end
-        else
-          clean_up_passwords resource
-          set_minimum_password_length
-          respond_with resource
-        end
       end
   end
 
