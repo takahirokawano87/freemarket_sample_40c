42cb6af494a4613a746141405c12b75c8d8dfddf
hanehane3710 <hanehane0464@gmail.com>
2019-01-04 15:43:38 +0900

    resource.saveならフォームを個人情報登録部分に渡すようにした

--- app/assets/javascripts/sign_up.js
+++ app/assets/javascripts/sign_up.js
@@ -1,6 +1,12 @@
 $(document).on('turbolinks:load', function() {
   $('.step2').hide();
 
+  var ResourceErrors = $('.errors').val();
+  if (ResourceErrors){
+    $('.step1').hide();
+    $('.step2').show();
+  }
+
   $('.first-form').on('click',function(){
     event.preventDefault();
     $('.step1').hide();
