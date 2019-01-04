1ffc7b828237671cc06ce95c8aa88c12fd3725ff
hanehane3710 <hanehane0464@gmail.com>
2019-01-04 15:44:18 +0900

    personal_infoテーブルのvalidationを組んだ

--- app/models/personal_info.rb
+++ app/models/personal_info.rb
@@ -4,4 +4,14 @@ class PersonalInfo < ApplicationRecord
   include JpPrefecture
   jp_prefecture :prefectures_code
 
+  validates :family_name, presence: true, length: {maximum: 4}, format: {with: /\A[一-龥]+\z/}
+  validates :first_name, presence: true, length: {maximum: 4}, format: {with: /\A[一-龥]+\z/}
+  validates :family_kana, presence: true, length: {maximum: 10}, format: {with: /\A[ァ-ンー－]+\z/}
+  validates :first_kana, presence: true, length: {maximum: 10}, format: {with: /\A[ァ-ンー－]+\z/}
+  validates :postal_code, presence: true, format: {with: /\A\d{3}[-]\d{4}\z/}
+  validates :prefectures_code, presence: true
+  validates :municipality, presence: true
+  validates :address_num, presence: true
+  validates :telephone, format: {with:  /\A\d{10}\z|\A\d{11}\z/}
+
 end
